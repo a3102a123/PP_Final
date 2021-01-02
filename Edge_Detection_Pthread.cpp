a13551c0c762f46ab67bfb4d5024215a1a9d5e9c
Barney <f0922571727@gmail.com>
2021-01-03 03:45:48 +0800

    Parrallel Pthread by seperate height (All)

--- Edge_Detection_Pthread.cpp
+++ Edge_Detection_Pthread.cpp
@@ -68,23 +68,6 @@ void print_fmatrix(int width, int height, int x, int y, int num, float *matrix)
   }
 }
 
-void ToGray(uint8_t *img, int width, int height, uint8_t *out_img)
-{
-  uint8_t *pixel, r, g, b;
-  int idx;
-  for (int j = 0; j < height; j++)
-  {
-    for (int i = 0; i < width; i++)
-    {
-      idx = (j * width + i);
-      pixel = img + idx * CHANNEL_NUM;
-      r = pixel[0];
-      g = pixel[1];
-      b = pixel[2];
-      out_img[idx] = (r * 30 + g * 59 + b * 11 + 50) / 100;
-    }
-  }
-}
 void *ToGrayThread(void *arg)
 {
   Arg *data = (Arg *)arg;
@@ -107,34 +90,6 @@ void *ToGrayThread(void *arg)
     }
   }
   pthread_exit((void *)0);
-}
-
-void Gaussian_blur(uint8_t *img, int width, int height, uint8_t *out_img)
-{
-  int idx;
-  for (int j = 0; j < height; j++)
-  {
-    for (int i = 0; i < width; i++)
-    {
-      idx = (j * width + i);
-      // set 0 to boundary
-      if (i == 0 || i == width - 1)
-      {
-        img[idx] = 0;
-        continue;
-      }
-      if (j == 0 || j == height - 1)
-      {
-        img[idx] = 0;
-        continue;
-      }
-      // Gaussian blur
-      float sum = 0;
-      for (int k = -4; k < 5; k++)
-        sum += (Blur_kernel[k + 4] * img[idx + k]);
-      out_img[idx] = sum / 16.0;
-    }
-  }
 }
 
 void *GaussianThread(void *arg)
@@ -167,46 +122,6 @@ void *GaussianThread(void *arg)
     }
   }
   pthread_exit((void *)0);
-}
-
-void Sobel_serial(uint8_t *img, int width, int height, float *angle, uint8_t *out_img)
-{
-  int idx;
-  for (int j = 0; j < height; j++)
-  {
-    for (int i = 0; i < width; i++)
-    {
-      idx = (j * width + i);
-      // set 0 to boundary
-      if (i == 0 || i == width - 1)
-      {
-        img[idx] = 0;
-        continue;
-      }
-      if (j == 0 || j == height - 1)
-      {
-        img[idx] = 0;
-        continue;
-      }
-      // Sobel edge detection
-      float sum_x = 0, sum_y = 0;
-      // x direction differential
-      for (int k = -4; k < 5; k++)
-        sum_x += (x_edge_kernel[k + 4] * img[idx + k]);
-      // y direction differential
-      for (int k = -4; k < 5; k++)
-        sum_y += (y_edge_kernel[k + 4] * img[idx + k]);
-      // the angle of gradient
-      angle[idx] = atan2f(sum_y, sum_x) * 180 / M_PI;
-      int sum = abs(sum_x) + abs(sum_y);
-      out_img[idx] = sum;
-      // thresholding
-      // if(sum >= 127)
-      //     out_img[idx] = 255;
-      // else
-      //     out_img[idx] = 0;
-    }
-  }
 }
 
 void *SobelThread(void *arg)
@@ -248,10 +163,13 @@ void *SobelThread(void *arg)
   pthread_exit((void *)0);
 }
 
-void non_max_Suppression(uint8_t *img, int width, int height, float *angle, uint8_t *out_img)
+void *SuppressionThread(void *arg)
 {
+  Arg *data = (Arg *)arg;
+  int tid = data->thread_id;
+
   int idx;
-  for (int j = 0; j < height; j++)
+  for (int j = chunk_height * tid; j < chunk_height * (tid + 1); j++)
   {
     for (int i = 0; i < width; i++)
     {
@@ -260,7 +178,7 @@ void non_max_Suppression(uint8_t *img, int width, int height, float *angle, uint
       {
         continue;
       }
-      if (j == 0 || j == height - 1)
+      if (j == 0 || j == chunk_height * (tid + 1) - 1)
       {
         continue;
       }
@@ -272,56 +190,64 @@ void non_max_Suppression(uint8_t *img, int width, int height, float *angle, uint
       // determine the adjacent gradient
       if ((dir >= 0 && dir < 22.5) || (dir >= 157.5 && dir <= 180))
       {
-        q = img[idx + width];
-        r = img[idx - width];
+        q = gradient_img[idx + width];
+        r = gradient_img[idx - width];
       }
       else if ((dir >= 22.5 && dir < 67.5))
       {
-        q = img[idx - width + 1];
-        r = img[idx + width - 1];
+        q = gradient_img[idx - width + 1];
+        r = gradient_img[idx + width - 1];
       }
       else if ((dir >= 67.5 && dir < 112.5))
       {
-        q = img[idx + 1];
-        r = img[idx - 1];
+        q = gradient_img[idx + 1];
+        r = gradient_img[idx - 1];
       }
       else if ((dir >= 112.5 && dir < 157.5))
       {
-        q = img[idx - width - 1];
-        r = img[idx + width + 1];
+        q = gradient_img[idx - width - 1];
+        r = gradient_img[idx + width + 1];
       }
       // supression
-      if (img[idx] >= q && img[idx] >= r)
-        out_img[idx] = img[idx];
+      if (gradient_img[idx] >= q && gradient_img[idx] >= r)
+        out_img[idx] = gradient_img[idx];
       else
         out_img[idx] = 0;
     }
   }
+  pthread_exit((void *)0);
 }
 
 // operate on same array
-void double_threshold(uint8_t *img, int width, int height)
+void *double_thresholdThread(void *arg)
 {
+  Arg *data = (Arg *)arg;
+  int tid = data->thread_id;
+
   int idx;
-  for (int j = 0; j < height; j++)
+  for (int j = chunk_height * tid; j < chunk_height * (tid + 1); j++)
   {
     for (int i = 0; i < width; i++)
     {
       idx = (j * width + i);
-      if (img[idx] >= STRONG)
-        img[idx] = 255;
-      else if (img[idx] >= WEEK)
-        img[idx] = WEEK;
+      if (out_img[idx] >= STRONG)
+        out_img[idx] = 255;
+      else if (out_img[idx] >= WEEK)
+        out_img[idx] = WEEK;
       else
-        img[idx] = 0;
+        out_img[idx] = 0;
     }
   }
+  pthread_exit((void *)0);
 }
 
-void Hysteresis(uint8_t *img, int width, int height)
+void *HysteresisThread(void *arg)
 {
+  Arg *data = (Arg *)arg;
+  int tid = data->thread_id;
+
   int idx;
-  for (int j = 0; j < height; j++)
+  for (int j = chunk_height * tid; j < chunk_height * (tid + 1); j++)
   {
     for (int i = 0; i < width; i++)
     {
@@ -330,29 +256,30 @@ void Hysteresis(uint8_t *img, int width, int height)
       {
         continue;
       }
-      if (j == 0 || j == height - 1)
+      if (j == 0 || j == chunk_height * (tid + 1) - 1)
       {
         continue;
       }
       idx = (j * width + i);
-      if (img[idx] == WEEK)
+      if (out_img[idx] == WEEK)
       {
         for (int k = -1; k <= 1; k++)
         {
           for (int l = -1; l <= 1; l++)
           {
-            if (img[idx + k * width + l] == 255)
+            if (out_img[idx + k * width + l] == 255)
             {
-              img[idx] = 255;
+              out_img[idx] = 255;
               break;
             }
           }
         }
       }
-      if (img[idx] != 255)
-        img[idx] = 0;
+      if (out_img[idx] != 255)
+        out_img[idx] = 0;
     }
   }
+  pthread_exit((void *)0);
 }
 
 int main(int argc, char **argv)
@@ -416,10 +343,6 @@ int main(int argc, char **argv)
   }
   gettimeofday(&end[1], NULL);
 
-  // gettimeofday(&start[1], NULL);
-  // Gaussian_blur(gray_img, width, height, blur_img);
-  // gettimeofday(&end[1], NULL);
-
   // Sobel Thread
   gettimeofday(&start[2], NULL);
   for (thread = 0; thread < number_of_thread; thread++)
@@ -432,20 +355,41 @@ int main(int argc, char **argv)
     pthread_join(thread_handles[thread], NULL);
   }
   gettimeofday(&end[2], NULL);
-  // gettimeofday(&start[2], NULL);
-  // Sobel_serial(blur_img, width, height, angle, gradient_img);
-  // gettimeofday(&end[2], NULL);
-
+  
   gettimeofday(&start[3], NULL);
-  non_max_Suppression(gradient_img, width, height, angle, out_img);
+  for (thread = 0; thread < number_of_thread; thread++)
+  {
+    arg[thread].thread_id = thread;
+    pthread_create(&thread_handles[thread], &attr, SuppressionThread, (void *)&arg[thread]);
+  }
+  for (thread = 0; thread < number_of_thread; thread++)
+  {
+    pthread_join(thread_handles[thread], NULL);
+  }
   gettimeofday(&end[3], NULL);
 
   gettimeofday(&start[4], NULL);
-  double_threshold(out_img, width, height);
+  for (thread = 0; thread < number_of_thread; thread++)
+  {
+    arg[thread].thread_id = thread;
+    pthread_create(&thread_handles[thread], &attr, double_thresholdThread, (void *)&arg[thread]);
+  }
+  for (thread = 0; thread < number_of_thread; thread++)
+  {
+    pthread_join(thread_handles[thread], NULL);
+  }
   gettimeofday(&end[4], NULL);
 
   gettimeofday(&start[5], NULL);
-  Hysteresis(out_img, width, height);
+  for (thread = 0; thread < number_of_thread; thread++)
+  {
+    arg[thread].thread_id = thread;
+    pthread_create(&thread_handles[thread], &attr, HysteresisThread, (void *)&arg[thread]);
+  }
+  for (thread = 0; thread < number_of_thread; thread++)
+  {
+    pthread_join(thread_handles[thread], NULL);
+  }
   gettimeofday(&end[5], NULL);
 
   stbi_write_png("result/image2.png", width, height, 1, out_img, width);
